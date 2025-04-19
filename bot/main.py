f5eb9097a0adec175daf79b163a50382de110b39
ZT <ztthingsgm@gmail.com>
2025-04-20 00:23:13 +0700

    Basic bot setup

---
+++ bot/main.py
@@ -0,0 +1,41 @@
+from dotenv import load_dotenv
+import os
+
+load_dotenv(dotenv_path="./config/.env")
+BOT_TOKEN = os.getenv('BOT_TOKEN')
+
+import discord
+from discord.ext import commands
+import sqlite3
+
+from settings import *
+
+intents = discord.Intents.all()
+bot = commands.Bot(command_prefix='!', intents=intents)
+
+def is_owner():
+    def predicate(ctx):
+        return ctx.author.id == OWNER
+    return commands.check(predicate)
+
+@bot.event
+async def on_ready():
+    print(f"Logged in as {bot.user}")
+    try:
+        synced = await bot.tree.sync()
+        print(f'Synced {len(synced)} command(s) to the bot.')
+    except Exception as e:
+        print(f'Error syncing commands: {e}')
+
+@bot.event
+async def on_guild_join(guild):
+    if guild.id != DQ_SERVER_ID:
+        print(f"Leaving unauthorized server: {guild.name}")
+        await guild.leave()
+
+@bot.command()
+@is_owner()
+async def hello(ctx):
+    await ctx.send(f"Hello {ctx.author.name}!")
+    
+bot.run(BOT_TOKEN)
