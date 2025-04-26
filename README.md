222c7a912ff330157826c520c9c0bfea9137638a
ZT <ztthingsgm@gmail.com>
2025-04-27 05:31:48 +0700

    Added README.md for known vulnerabilities

---
+++ README.md
@@ -0,0 +1,3 @@
+Known Vulnerabilities:
+- Won't update reactions for the correct range when there's a random message in daily question (Shouldn't be a problem)
+- Command misinput (No checks)
--- exp.py
+++
@@ -1,8 +0,0 @@
-from datetime import datetime
-from settings import START_DATE
-
-current_date = datetime.now()
-
-date_difference = (current_date - START_DATE).days + 1
-
-print(date_difference)
