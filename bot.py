959dfb70e65cf82a98f29acf675e59ac4c0152c1
ZT <ztthingsgm@gmail.com>
2025-04-27 07:22:58 +0700

    Clean up in bot.py

--- bot.py
+++ bot.py
@@ -6,13 +6,10 @@ BOT_TOKEN = os.getenv('BOT_TOKEN')
 
 import discord
 from discord.ext import commands
-import sqlite3
 
 from helper import *
 
 from apscheduler.schedulers.asyncio import AsyncIOScheduler
-import asyncio
-import datetime
 
 from settings import *
 
@@ -99,32 +96,3 @@ async def on_guild_join(guild):
     if guild.id != DQ_SERVER_ID:
         print(f"Leaving unauthorized server: {guild.name}")
         await guild.leave()
-
-# @bot.command()
-# @is_owner()
-# async def hello(ctx):
-#     await ctx.send(f"Hello {ctx.author.name}!")
-#     channel = bot.get_channel(DQ_CHANNEL_ID)
-#     message = await channel.send("BUH?")
-#     try:
-#         await message.publish()
-#     except Exception as e:
-#         print(f"Published went wrong: {e}")
-
-    
-
-    
-
-
-
-if __name__ == "__main__":
-    
-    # count = get_question_amount()
-
-    # print(count)
-    
-    # add_dq("Do you prefer cat or dog", "Cat%Dog")
-
-    
-
-    pass
